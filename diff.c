8b58b03d135dcdb7383b06593856708150a6b497
Nrj <84703848+Nrj27@users.noreply.github.com>
2022-08-07 09:21:03 +0530

    Add files via upload

---
+++ diff.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int x = 4 ;
+    int x = 5;
+    printf("%d",x);
+    return 0;
+}
+
+
+
+
+
+
+
+
+int main() 
+{
+    // Write C code here
+    int x;
+    x = 4 ;
+    x = 5;
+    printf("%d",x);
+    return 0;
+}
